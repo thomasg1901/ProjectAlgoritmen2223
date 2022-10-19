1fc0d60fa37e73736c39dc28de9f708ae11b7d28
jefjacobs00 <jacobsjef16@gmail.com>
2022-10-19 11:25:01 +0200

    add tests

--- src/Crane.java
+++ src/Crane.java
@@ -1,2 +1,21 @@
+import jdk.jshell.spi.ExecutionControl;
+
+import java.awt.*;
+
 public class Crane {
+    private Point possition;
+    private int speedx;
+    private int speedy;
+    public Crane(Point startPoint, int speedx, int speedy){
+        this.possition = startPoint;
+        this.speedx = speedx;
+        this.speedy = speedy;
+    }
+
+    public double moveCrane(Point p){
+
+        double timex = ((double)(p.x - possition.x))/speedx;
+        double timey = ((double) (p.y - possition.y))/speedy;
+        return 0.0;
+    }
 }
